37e5314efef165e4a32d14233042b80aa7ae0bdd
Annika <annika800@gmail.com>
2014-09-04 01:04:55 +0300

    baseline (or any other comment)

---
+++ src/main/java/org/test/controllers/DemoController.java
@@ -0,0 +1,15 @@
+package org.test.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("greetings")
+public class DemoController {
+	
+	@RequestMapping("en")
+	public String sayHello() {
+		return "index";
+		
+	}
+}
