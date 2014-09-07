3d423adc05e54ca20386004f628203fecb38f1e8
Annika <annika800@gmail.com>
2014-09-07 18:23:26 +0300

    Update for persistence support 3.2

--- src/main/java/org/test/repositories/PlantRepository.java
+++ src/main/java/org/test/repositories/PlantRepository.java
@@ -1,8 +1,11 @@
 package org.test.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.test.models.Plant;
 
+@Repository
 public interface PlantRepository extends JpaRepository<Plant, Long> {
 
+	
 }
